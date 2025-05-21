3c59226d9b2b52f210e08d9a1e0e959c33a4e43e
Riley Satanek <rileysatanek@Rileys-MacBook-Pro.local>
2025-05-21 19:34:54 +1000

    remove deleted files

--- src/sesService.js
+++ src/sesService.js
@@ -1,32 +1,72 @@
 const { SESClient, SendEmailCommand } = require('@aws-sdk/client-ses');
+const stockService = require('./stockService');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 class SesService {
   constructor() {
-    this.fromEmail = process.env.FROM_EMAIL;
+    // gmail
+    this.gmailUser = process.env.GMAIL_EMAIL;
+    this.gmailPassword = process.env.GMAIL_PASSWORD;
+
+    // ses
+    this.sesEmail = process.env.SES_EMAIL;
+
     this.toEmail = process.env.USER_EMAIL;
     this.useMock = process.env.USE_MOCK_EMAIL === "true";
 
+    // this.isLambda = !!process.env.AWS_LAMBDA_FUNCTION_NAME;
+    this.isLocal = !Boolean(process.env.AWS_LAMBDA_FUNCTION_NAME);
+
     this.ses = null;
+    this.transporter = null;
     this.initializeEmailClient();
   }
 
   initializeEmailClient() {
-    if (!this.useMock && this.fromEmail && this.toEmail) {
+    if (!this.useMock && this.isLocal && this.gmailPassword && this.gmailUser) { // if local mode
+      try {
+        console.log('Local mode - Initialising gmail transport')
+        this.transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: this.gmailUser,
+                pass: this.gmailPassword
+            }
+        })
+      } catch (error) {
+        console.error('Error initializing gmail transporter client:', error.message);
+        this.useMock = true;
+      }
+    } else if (!this.useMock && !this.isLocal && this.sesEmail) { // if lambda mode
       try {
         this.ses = new SESClient({ region: process.env.AWS_REGION || 'us-east-1' });
         console.log('SES client initialized successfully');
       } catch (error) {
         console.error('Error initializing SES client:', error.message);
-        this.ses = null;
+        this.useMock = true;
       }
     } else {
-      console.log('Email configuration missing or mock mode enabled. Email will be simulated.');
+      console.log('Mock mode enabled. Email will be simulated.');
+      this.useMock = true;
     }
+
+
+    // if (!this.useMock && this.fromEmail && this.toEmail) {
+    //   try {
+    //     this.ses = new SESClient({ region: process.env.AWS_REGION || 'us-east-1' });
+    //     console.log('SES client initialized successfully');
+    //   } catch (error) {
+    //     console.error('Error initializing SES client:', error.message);
+    //     this.ses = null;
+    //   }
+    // } else {
+    //   console.log('Email configuration missing or mock mode enabled. Email will be simulated.');
+    // }
   }
 
-  formatPerformanceMessage(performanceData) {
-    const { stocks, totalCurrentValue, totalValueChange, totalChangePercent, date } = performanceData;
+  async formatPerformanceMessage(performanceData) {
+    const { stocks, totalCurrentValue, totalValueChange, totalChangePercent, overallValueChange, overallChangePercent, date } = performanceData;
 
     const formattedDate = new Date(date).toLocaleDateString('en-AU', {
       weekday: 'long',
@@ -39,29 +79,52 @@ class SesService {
 
     let htmlMessage = `<h2>📈 Daily Stock Summary</h2><hr>`;
 
-    stocks.forEach(stock => {
+    // const stocksPerformance = portfolio.stocks.map(portfolioStock =>
+
+    for (const stock of stocks) {
+      let currentPrice, currentValue, overallDiff;
+
+      currentPrice = stock.currentPrice;
+      currentValue = stock.currentValue;
+      overallDiff = stock.overallDiff;
+
+
       const isUp = stock.valueChange >= 0;
       const changeSymbol = isUp ? '📈' : '📉';
       const changeSign = isUp ? '+' : '';
       const color = isUp ? 'green' : 'red';
 
+      const isUpOverall = stock.overallChange >= 0;
+      const colorOverall = isUpOverall ? 'green' : 'red';
+      const changeSignOverall = isUpOverall ? '+' : '';
+
       htmlMessage += `
         <h3>${changeSymbol} ${stock.symbol} (${stock.name})</h3>
-        <p><strong>Price:</strong> <span style="color:${color};">$${stock.currentPrice.toFixed(2)} (${changeSign}${stock.changePercent.toFixed(2)}%)</span><br>
-        <strong>Value:</strong> <span style="color:${color};">$${stock.currentValue.toFixed(2)} (${changeSign}$${stock.valueChange.toFixed(2)})</span></p>
+        <p>
+        <strong>Price:</strong> <span style="color:${color};">$${currentPrice.toFixed(2)} (${changeSign}${stock.changePercent.toFixed(2)}%)</span><br>
+        <strong>Value:</strong> <span style="color:${color};">$${currentValue.toFixed(2)} (${changeSign}$${stock.valueChange.toFixed(2)})</span><br>
+        <strong>Overall:</strong> <span style="color:${colorOverall};">$${overallDiff.toFixed(2)} (${changeSignOverall}${stock.overallChange.toFixed(2)}%)</span>
+        </p>
       `;
-    });
+    }
 
     const totalUp = totalValueChange >= 0;
     const totalChangeSymbol = totalUp ? '📈' : '📉';
     const totalChangeSign = totalUp ? '+' : '';
     const totalColor = totalUp ? 'green' : 'red';
 
+    const overallUp = overallValueChange >= 0;
+    const overallChangeSymbol = overallValueChange ? '📈' : '📉';
+    const overallChangeSign = overallUp ? '+' : '';
+    const overallColor = overallUp ? 'green' : 'red';
+
     htmlMessage += `
       <hr>
       <h2>📊 Portfolio Summary</h2>
       <p><strong>Total Value:</strong> $${totalCurrentValue.toFixed(2)}<br>
-      <strong>Daily Change:</strong> <span style="color:${totalColor};">${totalChangeSymbol} ${totalChangeSign}$${totalValueChange.toFixed(2)} (${totalChangeSign}${totalChangePercent.toFixed(2)}%)</span></p>
+      <strong>Daily Change:</strong> <span style="color:${totalColor};">${totalChangeSymbol} ${totalChangeSign}$${totalValueChange.toFixed(2)} (${totalChangeSign}${totalChangePercent.toFixed(2)}%)</span><br>
+      <strong>Overall Change:</strong> <span style="color:${overallColor};">${overallChangeSymbol} ${overallChangeSign}$${overallValueChange.toFixed(2)} (${totalChangeSign}${overallChangePercent.toFixed(2)}%)</span>
+      </p>   
     `;
 
     return { subject, htmlMessage };
@@ -69,15 +132,15 @@ class SesService {
 
   async sendPerformanceUpdate(performanceData) {
     try {
-      const { subject, htmlMessage } = this.formatPerformanceMessage(performanceData);
+      const { subject, htmlMessage } = await this.formatPerformanceMessage(performanceData);
 
-      if (!this.ses) {
-        console.log('SES client not initialized. Message would have been:');
+      if (this.useMock) {
+        console.log('Emailer client not initialized. Message would have been:');
         console.log(htmlMessage);
         return { status: 'simulated', htmlMessage };
-      }
-
-      const params = {
+      } else if (this.ses) {
+        console.log('Sending email from SES');
+        const params = {
         Destination: {
           ToAddresses: [this.toEmail],
         },
@@ -93,13 +156,31 @@ class SesService {
             Data: subject,
           },
         },
-        Source: this.fromEmail,
+        Source: this.sesEmail,
       };
 
       await this.ses.send(new SendEmailCommand(params));
 
       console.log('Email sent via SES');
       return { status: 'sent', htmlMessage };
+
+      } else if (this.transporter) {
+        console.log('Sending email from nodemailer');
+        const mailOptions = {
+            from: this.gmailUser,
+            to: this.toEmail,
+            subject: subject,
+            html: htmlMessage
+        };
+
+        this.transporter.sendMail(mailOptions);
+
+        console.log(`Email sent`);
+        return { status: 'sent', htmlMessage };
+
+      } else {
+        console.log("No emailer setup")
+      }
     } catch (error) {
       console.error('Error sending email via SES:', error.message);
       return { status: 'error', error: error.message, htmlMessage: null };
